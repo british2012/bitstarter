fffa550b9ac280dfd3b225e8c18cb1a63edcc0cb
Britianny Walker <britianny@teambakari.com>
2013-07-25 20:34:06 +0000

    Unstable - Test web.js printing string from index.html

--- web.js
+++ web.js
@@ -2,12 +2,13 @@ var express = require('express');
 var app = express();
 
 var fs = require('fs');
+var output = fs.readFileSync('index.html', 'utf8');
 
 app.use(express.logger());
 
 app.get('/', function(request, response) {
- // response.send('Hello World2!');
-    fs.readFileSync('index.html', 'utf8');
+  //response.send('Hello World2!');
+    response.send(output);
 });
 
 var port = process.env.PORT || 5000;
